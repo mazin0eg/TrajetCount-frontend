abc914f717be82487088bd8960a48df30464c7df
mazin0eg <mazin.zougui@gmail.com>
2025-12-10 12:16:37 +0100

    feature: not  found page fait

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from './components/register.jsx';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
+import NotFound from './components/notfound.jsx';
 
 const App = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-
+          <Route path="*" element={<NotFound />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
---
+++ src/components/notfound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className='flex justify-center  items-center'>
+        <h1>not found </h1>
+        <a href="/home">go to home page </a>
+    </div>
+  )
+}
